c4b50d921801ce053e1ee2d823a73c4e755c6242
MoskvaRodion <74708306+MoskvaRodion@users.noreply.github.com>
2022-06-06 21:07:16 +0300

    master

--- main.php
+++ main.php
@@ -1,11 +1,6 @@
 <?php
 $inform = new PDO("sqlite:Countries.sqlite");
-
 $sql = "SELECT * FROM 'informations' LIMIT 10";
-
 $result = $inform->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($result as $item){
-    echo $item['country_name'] . + "-" . $item['language']. + "-" . $item['Capital'];
-}
+print_r($result);
 ?>   
