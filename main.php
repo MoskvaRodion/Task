19ead3a2c5fccec88a030477a8531d12d155809f
MoskvaRodion <74708306+MoskvaRodion@users.noreply.github.com>
2022-06-07 11:07:14 +0300

    master

--- main.php
+++ main.php
@@ -2,5 +2,7 @@
 $inform = new PDO("sqlite:Countries.sqlite");
 $sql = "SELECT * FROM 'informations' LIMIT 10";
 $result = $inform->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+echo "<pre>";
 print_r($result);
+echo "</pre>";
 ?>   
