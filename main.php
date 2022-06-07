14b033d5f06d79329fb07bd1b4f7b4fd726f4b72
MoskvaRodion <74708306+MoskvaRodion@users.noreply.github.com>
2022-06-07 15:18:41 +0300

    master

--- main.php
+++ main.php
@@ -2,7 +2,27 @@
 $inform = new PDO("sqlite:Countries.sqlite");
 $sql = "SELECT * FROM 'informations' LIMIT 10";
 $result = $inform->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+
 echo "<pre>";
 print_r($result);
 echo "</pre>";
-?>   
+?>
+<table>
+    <thead>
+        <tr>
+            <td>№</td>
+            <td>Название страны</td>
+            <td>Официальный язык</td>
+            <td>Столица</td>
+        </tr>
+    </thead>
+<?php foreach($data as $value){ ?>
+    <tr>
+        <td><?php echo $value['Id']; ?></td>
+        <td><?php echo $value['country_name']; ?></td>
+        <td><?php echo $value['language']; ?></td>
+        <td><?php echo $value['capital']; ?></td>
+    </tr>
+<?php } ?>
+</table>
